1ec6b942638828e2a5ab32aba35942aadd3c7671
elinaelbrink <e_elbrink@hotmail.com>
2023-06-02 15:57:54 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+//GET api
+const apiUrl = "https://reqres.in/api/users/";
+const userContainer = document.getElementById("user-container");
+
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        const users = data.data;
+
+        users.forEach(user => {
+            const userUl = document.createElement("ul");
+            userUl.innerHTML = `
+            <div class="user">
+            <button id="openModal">
+            <img src="${user.avatar}" alt="profile_picture">
+            <h4>${user.first_name} ${user.last_name}</h4>
+            </button>
+            </div>
+          `;
+            userContainer.appendChild(userUl);
+        });
+    })
+    .catch(error => {
+        console.log("Failed to fetch data:", error);
+    });
+
+//Modal
+
+
